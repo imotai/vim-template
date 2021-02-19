2dccaa437b9fd53fa017e01c74c977ff32ce409f
imotai <wangtaize@4paradigm.com>
2021-02-19 10:59:36 +0800

    feat: update c/c++ template

--- templates/=template=.c
+++ templates/=template=.c
@@ -1,9 +1,20 @@
-/*
- * %FFILE%
- * Copyright (C) %YEAR% %USER% <%MAIL%>
- *
- * Distributed under terms of the %LICENSE% license.
- */
+//
+//
+// %FFILE%
+// Copyright (C) %YEAR% 4Paradigm Author %USER% <%MAIL%>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
 #include "%FILE%.h"
 
